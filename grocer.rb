9610e9317e10b7d31edeba769247a1d51040d84a
David Eber <deber116@gmail.com>
2020-02-03 07:35:29 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,18 @@
+require 'pp'
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  collection_index = 0
+  result = nil
+  while collection_index < collection.count do 
+    if collection[collection_index][:item] == name
+      result = collection[collection_index]
+    end
+    collection_index += 1
+  end
+  result
 end
 
 def consolidate_cart(cart)
@@ -9,19 +20,82 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
+  result = []
+  cart_index = 0
+  while cart_index < cart.count do 
+    addition_to_result = cart[cart_index]
+    inner_cart_index = 0 
+    item_count = 0 
+    while inner_cart_index < cart.count do 
+      if cart[cart_index] == cart[inner_cart_index]
+        item_count += 1
+      end
+      inner_cart_index += 1
+    end
+    addition_to_result[:count] = item_count
+    result << addition_to_result
+    cart_index += 1
+  end
+  result.uniq
 end
+
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart_index = 0 
+  final_cart = []
+  while cart_index < cart.count do 
+    coupon_index = 0 
+    while coupon_index < coupons.count do 
+      if cart[cart_index][:item] == coupons[coupon_index][:item]
+        cart_item = cart[cart_index][:item]
+        cart_clearance = cart[cart_index][:clearance]
+        cart_count = cart[cart_index][:count]
+        coupon_price = coupons[coupon_index][:cost]
+        coupon_num = coupons[coupon_index][:num]
+        full_price_items = cart_count % coupon_num
+        count_items_coupon_price = cart_count - full_price_items
+        cart[cart_index][:count] = full_price_items
+        coupon_price_per_item = coupon_price / coupon_num
+        cart << {
+          item: cart_item + " W/COUPON",
+          price: coupon_price_per_item,
+          clearance: cart_clearance,
+          count: count_items_coupon_price
+        }
+      end
+      coupon_index += 1
+    end
+    cart_index += 1
+  end
+  cart
 end
+#itterate through each item in the cart and compare it to the coupons array to see if there are any matches 
+#if there are any matches, full_price_items = :count % :num 
+#loop result = :count - :num until result == full_price_items and count the number of times the loop is run and then multiply it by :num to get the total count of coupon items
+#update the count of full price items
+#add updated full price items to final result 
+#add coupon items to final result
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  cart_index = 0 
+  while cart_index < cart.count do 
+    if cart[cart_index][:clearance] == true
+      updated_price = cart[cart_index][:price] * 0.8
+      cart[cart_index][:price] = updated_price.round(2)
+    end
+    cart_index += 1
+  end
+  cart
 end
+#itterate through each item to check if it is on clearance 
+#if it's on clearance, update the price to be 20% less 
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
@@ -33,4 +107,22 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidated = consolidate_cart(cart)
+  consolidated = apply_coupons(consolidated,coupons)
+  consolidated = apply_clearance(consolidated)
+  grand_total = 0
+  total_index = 0 
+  while total_index < consolidated.count do 
+    item_price = consolidated[total_index][:price]
+    item_count = consolidated[total_index][:count]
+    item_total = item_price * item_count
+    grand_total += item_total
+    total_index += 1 
+  end
+  if grand_total > 100
+    grand_total = grand_total * 0.9
+    grand_total.round(2)
+  end
+  grand_total
 end
+#[{:item=>"BEETS", :price=>2.5, :clearance=>false, :count=>1}]
